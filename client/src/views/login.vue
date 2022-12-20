270dd0b10750a6c06f1202b1ddb0921b974d9f13
nnj-wp <nj448jack@gmail.com>
2022-12-20 17:01:38 +0800

    add server, connect postgreSQL

---
+++ client/src/views/login.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card width="500px" color="white" class="mx-auto mt-5">
+        <v-card-title class="pt-8">
+            <h1>登入</h1>    
+        </v-card-title>
+        <v-card-text>
+           <v-from>
+                <v-text-field
+                label="帳號"
+                prepend-icon="mdi-account-circle"
+                v-model="useraccount"
+                autofocus
+                />
+            </v-from> 
+
+        </v-card-text>
+        <v-card-actions class="pb-3">
+            <v-btn color="primary" @click="login" class="mx-3">登入</v-btn>
+
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+
+<script>
+export default {
+
+    data(){
+        return{
+            useraccount:'',
+        }
+    },
+    methods:{
+        login(){
+            alert(this.useraccount)
+        }
+    }
+}
+</script>
+
+
+<style>
+
+.mx-auto {
+    height: 5px;
+}
+
+</style>
---
+++ server/utils/database.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'testdb',
+  password: '209410751',
+  port: '5432',
+});
+
+pool.query('SELECT * from test', (err, res) => {
+  console.log(JSON.stringify(res.rows));
+  pool.end();
+});
